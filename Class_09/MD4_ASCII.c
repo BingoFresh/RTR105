0fcd6a9b09c80657d22c0996ba1838d30e06012e
BingoFresh <localfalls@yahoo.com>
2022-10-21 11:45:48 +0300

    Class_04

---
+++ Class_09/MD4_ASCII.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+
+	char b = 66;
+		printf ("b mainīgā vērtība (kā simbols) pēc piešķiršanas: %c\n  ",b);
+		printf ("b mainīgā vērtība (kā dec skaitlis) pēc piešķiršanas: %d\n  ",b);
+		printf ("b mainīgā vērtība (kā hex skaitlis) pēc piešķiršanas: %#x\n  ",b);
+		printf ("b mainīgā vērtība (kā oct skaitlis) pēc piešķiršanas: %#o\n  ",b);
+
+	char f = 70;
+		printf ("f mainīgā vērtība (kā simbols) pēc piešķiršanas: %c\n  ",f);
+		printf ("f mainīgā vērtība (kā dec skaitlis) pēc piešķiršanas: %d\n  ",f);
+		printf ("f mainīgā vērtība (kā hex skaitlis) pēc piešķiršanas: %#x\n  ",f);
+		printf ("f mainīgā vērtība (kā oct skaitlis) pēc piešķiršanas: %#o\n  ",f);
+
+return 0;
+}
---
+++ Class_11/if_tests.c
@@ -0,0 +1,24 @@
+//nosacījuma operators if tiek izmantots koda izpildes sazarošanai
+//if (izteiksme) darbiiba_1_gab;
+//if (izteiksme)
+// darbiiba_1_gab;
+//nozīmē sekojošo - darbiiba_1_gab tiks izpildīta tad, kad izteiksmes izpildīšanās rezultāts ir "true"
+//if (izteiksme) {1_darbiiba; 2_darbiiba; ...;}
+//if (izteiksme)
+// {
+// 1_darbiiba;
+// 2_darbiiba;
+// }
+// ar {} apzīmē darbību bloku
+//if (izteiksme) darbiiba_1_gab; else darbiiba_2_gab;
+//if
+// darbiiba_1_gab;
+//else
+// darbiiba_2_gab;
+
+#include<stdio.h>
+
+int main()
+{
+
+}
